fdc6cabc4b359babde640321df98add4183a58a9
SakRoi <roininensaku@gmail.com>
2023-05-08 10:41:07 +0300

    FEAT: change backend to work without HTTPS

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const jwt = require('jsonwebtoken');
-const https = require("https");
+//const https = require("https");
 const fs = require("fs");
 
 const dotenv=require('dotenv');
@@ -34,7 +34,12 @@ app.use('/accounts',accountsRouter);
 app.use('/history',historyRouter);
 app.use('/ownership', ownershipRouter);
 
-const options = {
+app.listen(process.env.port, function(){
+    console.log("Application listens to port " + process.env.port);
+});
+
+//commented code is for HTTPS version
+/*const options = {
 	key: fs.readFileSync(process.env.privkey, 'utf8'),
 	cert: fs.readFileSync(process.env.cert, 'utf8'),
 	ca: fs.readFileSync(process.env.chain, 'utf8')
@@ -44,7 +49,7 @@ const httpsServer = https.createServer(options, app);
 
 httpsServer.listen(3000, () => {
 	console.log("Application listening to port " + process.env.port);
-});
+});*/
 
 
 //Source: https://peatutor.com/express/Examples/webtoken.php
