8a5e4c2a756ed3e81bdbbccbe9e4ee560d200fc1
SakRoi <roininensaku@gmail.com>
2023-05-08 15:53:38 +0300

    FEAT: Add admin controller and model

---
+++ backend/models/admin_model.js
@@ -0,0 +1,13 @@
+const db = require('../database');
+
+const admin = {
+    checkPassword: function(username, callback) {
+        return db.query("select password from administrators where userName = ?", [username], callback);
+    },
+
+    checkRoot: function(callback) {
+        return db.query("select * from administrators where userName = root", callback);
+    }
+}
+
+module.exports=admin;
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -3,43 +3,61 @@ const router = express.Router();
 const jtw = require('jsonwebtoken');
 const dotenv = require('dotenv');
 const bcrypt = require('bcryptjs');
-const login = require("../models/admin_model");
+const admin = require("../models/admin_model");
 
 //Original source: https://peatutor.com/express/Examples/webtoken.php, refactored by Saku Roininen
 router.post('/',
+
 function(request, response) {
-    if(!(request.body.cardID && request.body.PINcode)){
+    if(!(request.body.username && request.body.password)){
         console.log("username or password missing");
         response.send(false);
-    } else {
-        const user = request.body.cardID;
-        const pass = request.body.PINcode;
-            
-        login.checkPin(user, function(dbError, dbResult) {
-            if(dbError){
-                response.json(dbError);
-            } else if (!(dbResult.length > 0)) {
-                console.log("user does not exists");
+    } else if(request.body.username === "root" && request.body.password === "root"){
+
+    /*this is the default root username and password. 
+    When they first log in to the admin control panel, they are asked to 
+    either remove the "root" account or change it's password for safety purposes.*/
+
+        admin.checkRoot(function(dbError, dbResult){
+            if(dbError || !(dbResult.length > 0)){
+                console.log("Root Account does not exist");
                 response.send(false);
             } else {
-                bcrypt.compare(pass,dbResult[0].PINcode, function(error,compareResult) {
+                console.log("Success");
+                user = request.body.username;
+                const token = generateAccessToken({ cardID: user }, { role: 2802});
+                response.send(token);
+            }
+        });
+    } else {
+        const user = request.body.username;
+        const pass = request.body.password;
+
+        admin.checkPassword(user, function(dbError, dbResult){
+            if(dbError){
+                response.json(dbError);
+            } else if (!(dbResult.length > 0)){
+                console.log("user does not exist");
+                response.send(false);
+            } else {
+                bcrypt.compare(pass,dbResult[0].password, function(error,compareResult) {
                     if(!compareResult) {
                         console.log("wrong password");
                         response.send(false);
                     } else {
                     console.log("success");
-                    const token = generateAccessToken({ cardID: user });
+                    const token = generateAccessToken({ cardID: user }, { role: 2802});
                     response.send(token);
-                    }		
+                    }
                 });
             }
         });
     }
 });
 
-function generateAccessToken(id) {
+function generateAccessToken(id, role) {
     dotenv.config();
-    return jtw.sign(id, process.env.MY_TOKEN, { expiresIn: '200s'});
+    return jtw.sign(id, role, process.env.MY_TOKEN, { expiresIn: '200s'});
 }
 
 module.exports = router;
